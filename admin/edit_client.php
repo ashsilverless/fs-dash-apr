ebcb32e3184a28f95ad9161cb98b7655877ee354
Ash - SIlverless <ash@silverless.co.uk>
2020-04-27 13:14:04 +0100

    DEV - edit client screen

--- admin/edit_client.php
+++ admin/edit_client.php
@@ -56,121 +56,96 @@ require_once('page-sections/header-elements.php');
     <div class="border-box main-content">
         <h1 class="heading heading__2">Client Details</h1>
 		<form action="editclient.php?id=<?=$client_id;?>" method="post" id="editclient" name="editclient" class="asset-form">
-            <div class="content">
-                <div class="item">
-                    <label>Client Name</label>
-                    <input type="text" id="client_name" name="client_name" value="<?=$user_name;?>">
+            <div class="content client">
+                <div class="client__pers-details">
+                    <div class="item half mb1">
+                        <label>Client Name</label>
+                        <input type="text" id="client_name" name="client_name" value="<?=$user_name;?>">
+                    </div>
+                    <div class="item half">
+                        <label>Client Email</label>
+                        <input type="text" id="client_email" name="client_email" value="<?=$email_address;?>">
+                    </div>
+                    <div class="item mb1">
+                        <label>User ID</label>
+                            <input type="text" id="fs_client_code" name="fs_client_code" value="<?=$fs_client_code;?>">
+                    </div>
+                    <div class="item">
+                        <label>Strategy</label>
+                            <div class="select-wrapper">
+                            <select name="strategy" id="strategy" class="select-css">
+                              <option value="Sensible" <?php if(strtolower ($strategy)=='sensible'){?>selected = 'selected' <?php }?>>Sensible</option>
+                              <option value="Steady" <?php if(strtolower ($strategy)=='steady'){?>selected = 'selected' <?php }?>>Steady</option>
+                              <option value="Serious" <?php if(strtolower ($strategy)=='serious'){?>selected = 'selected' <?php }?>>Serious</option>
+                            </select>
+                            <i class="fas fa-sort-down"></i>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <label>Client Type</label>
+                        <div class="select-wrapper">
+                            <select name="fs_client_desc" id="fs_client_desc" class="select-css">
+                              <option value="Private Client" <?php if(strtolower ($desc)=='private client'){?>selected = 'selected' <?php }?>>Private</option>
+                              <option value="Corporate Client" <?php if(strtolower ($desc)=='corporate client'){?>selected = 'selected' <?php }?>>Corporate</option>
+                            </select>
+                            <i class="fas fa-sort-down"></i>
+                        </div>
+
+                    </div>
+                    <div></div>
+                </div><!--pers details-->
+                <div class="client__pers-accounts">
+                    <h3 class="heading heading__2">Accounts</h3>
+                    <a href="#" class="addasset button button__raised button__inline">Add Account</a>
+                    <div class="recess-box">
+                    <div class="account-table">
+                        <div class="account-table__head">
+                            <label>Client Code</label>
+                            <label>ISIN Code</label>
+                            <label>Designator</label>
+                            <label>Type</label>
+                            <label>Display Name</label>
+                        </div>
+                        <?php foreach($products as $product) { ?>
+                        <div class="account-table__body">
+                            <input type="text" id="fs_client_code<?=$product['id'];?>" name="fs_client_code<?=$product['id'];?>" value="<?=$product['fs_client_code'];?>" readonly>
+
+                            <input type="text" id="fs_isin_code<?=$product['id'];?>" name="fs_isin_code<?=$product['id'];?>" value="<?=$product['fs_isin_code'];?>" readonly>
+
+                            <input type="text" id="designator<?=$product['id'];?>" name="designator<?=$product['id'];?>" value="<?=$product['fs_designation'];?>" readonly>
+
+                            <select name="product_type<?=$product['id'];?>" id="product_type<?=$product['id'];?>">
+                                <option value="ISA" <?php if(strtolower ($product['fs_product_type'])=='isa'){?>selected = 'selected' <?php }?>>ISA</option>
+                                <option value="JISA" <?php if(strtolower ($product['fs_product_type'])=='jisa'){?>selected = 'selected' <?php }?>>JISA</option>
+                                <option value="PIA" <?php if(strtolower ($product['fs_product_type'])=='pia'){?>selected = 'selected' <?php }?>>PIA</option>
+                                <option value="SIPP" <?php if(strtolower ($product['fs_product_type'])=='sipp'){?>selected = 'selected' <?php }?>>SIPP</option>
+                                <option value="Unwrapped" <?php if(strtolower ($product['fs_product_type'])=='unwrapped'){?>selected = 'selected' <?php }?>>Unwrapped</option>
+                            </select>
+
+                            <input type="text" id="display_name<?=$product['id'];?>" name="display_name<?=$product['id'];?>" value="<?=$product['fs_client_name'] . ' ' . $product['fs_product_type'];?>" readonly>
+
+                        </div>
+                         <?php } ?>
+                    </div>
                 </div>
-                <div class="item">
-                    <label>CLeint Email</label>
-                    <input type="text" id="client_email" name="client_email" style="width:90%" value="<?=$email_address;?>">
                 </div>
-                <div class="item">
-
-                </div>
-
-                <div class="item">
-
-                </div>
-
-            </div>
-        <div class="control">
-            <h3 class="heading heading__2">Account Actions</h3>
-        </div>
-</div>
-</div>
-
-
-			<div class="col-md-6" style="float:left;">
-				<p></p>
-			</div>
-
-			<div class="col-md-3" style="float:left;">
-				<p>User ID<br>
-					<input type="text" id="fs_client_code" name="fs_client_code" style="width:90%" value="<?=$fs_client_code;?>"></p>
-			</div>
-
-			<div class="col-md-3" style="float:left;">
-				<p>Strategy<br>
-					<select name="strategy" id="strategy">
-					  <option value="Sensible" <?php if(strtolower ($strategy)=='sensible'){?>selected = 'selected' <?php }?>>Sensible</option>
-					  <option value="Steady" <?php if(strtolower ($strategy)=='steady'){?>selected = 'selected' <?php }?>>Steady</option>
-					  <option value="Serious" <?php if(strtolower ($strategy)=='serious'){?>selected = 'selected' <?php }?>>Serious</option>
-					</select>
-			   </p>
-			</div>
-
-			<div class="col-md-3" style="float:left;">
-				<p>Client Type<br>
-					<select name="fs_client_desc" id="fs_client_desc">
-					  <option value="Private Client" <?php if(strtolower ($desc)=='private client'){?>selected = 'selected' <?php }?>>Private</option>
-					  <option value="Corporate Client" <?php if(strtolower ($desc)=='corporate client'){?>selected = 'selected' <?php }?>>Corporate</option>
-					</select>
-				</p>
-			</div>
-
-			<div class="col-md-3" style="float:left;">
-				<p>Mobile Phone (for 2FA)<br>
-					<input type="text" id="telephone" name="telephone" style="width:90%" value="<?=$telephone;?>"></p>
-			</div>
-
-			<div class="col-md-8 offset-2 mt-3 mb-3"><hr></div>
-
-			<h4>Accounts</h4>
-
-			<div class="col-md-12  table-responsive mt-5">
-			  <table class="table table-sm table-striped">
-			    <tbody>
-					<tr>
-				      <td width="15%" bgcolor="#FFFFFF"><strong>Client Code</strong></td>
-					  <td width="20%" bgcolor="#FFFFFF"><strong>ISIN Code</strong></td>
-					  <td width="20%" bgcolor="#FFFFFF"><strong>Designator</strong></td>
-					  <td width="15%" bgcolor="#FFFFFF"><strong>Type</strong></td>
-					  <td width="30%" bgcolor="#FFFFFF"><strong>Display Name</strong></td>
-				  </tr>
-
-			<?php foreach($products as $product) { ?>
-				<tr>
-					<td><input type="text" id="fs_client_code<?=$product['id'];?>" name="fs_client_code<?=$product['id'];?>" style="width:90%" value="<?=$product['fs_client_code'];?>" readonly></td>
-					<td><input type="text" id="fs_isin_code<?=$product['id'];?>" name="fs_isin_code<?=$product['id'];?>" style="width:90%" value="<?=$product['fs_isin_code'];?>" readonly></td>
-					<td><input type="text" id="designator<?=$product['id'];?>" name="designator<?=$product['id'];?>" style="width:90%" value="<?=$product['fs_designation'];?>" readonly></td>
-					<td><select name="product_type<?=$product['id'];?>" id="product_type<?=$product['id'];?>">
-							<option value="ISA" <?php if(strtolower ($product['fs_product_type'])=='isa'){?>selected = 'selected' <?php }?>>ISA</option>
-							<option value="JISA" <?php if(strtolower ($product['fs_product_type'])=='jisa'){?>selected = 'selected' <?php }?>>JISA</option>
-							<option value="PIA" <?php if(strtolower ($product['fs_product_type'])=='pia'){?>selected = 'selected' <?php }?>>PIA</option>
-							<option value="SIPP" <?php if(strtolower ($product['fs_product_type'])=='sipp'){?>selected = 'selected' <?php }?>>SIPP</option>
-							<option value="Unwrapped" <?php if(strtolower ($product['fs_product_type'])=='unwrapped'){?>selected = 'selected' <?php }?>>Unwrapped</option>
-						</select></td>
-					<td><input type="text" id="display_name<?=$product['id'];?>" name="display_name<?=$product['id'];?>" style="width:90%" value="<?=$product['fs_client_name'] . ' ' . $product['fs_product_type'];?>" readonly></td>
-				</tr>
-			  <?php } ?>
-			   </tbody>
-			  </table>
-			</div>
-
-
-			<div class="col-md-8 offset-2 mt-3 mb-3"><hr></div>
-
-			<h4>Linked Accounts</h4>
-
-
-			<?php if($linked_accounts!=''){ $lnk_array = explode('|',$linked_accounts);?>
-
-				<?php for($b=0;$b<count($lnk_array);$b++){
-                     if($lnk_array[$b]!=''){  ?>
-					<p><strong>Linked Account Holder :</strong> <?=getUserName($lnk_array[$b])?></p>
-					<table class="table table-sm table-striped">
-						<tbody>
-							<tr>
-							  <td width="15%" bgcolor="#FFFFFF"><strong>Client Code</strong></td>
-							  <td width="20%" bgcolor="#FFFFFF"><strong>ISIN Code</strong></td>
-							  <td width="20%" bgcolor="#FFFFFF"><strong>Designator</strong></td>
-							  <td width="15%" bgcolor="#FFFFFF"><strong>Type</strong></td>
-							  <td width="30%" bgcolor="#FFFFFF"><strong>Display Name</strong></td>
-							</tr>
-
-						  <?php
-
+                <div class="client__linked-accounts">
+                    <h3 class="heading heading__2">Linked Accounts</h3>
+                    <a href="#" class="addasset button button__raised button__inline">Add Linked Account</a>
+                    <div class="account-table">
+                        <?php if($linked_accounts!=''){ $lnk_array = explode('|',$linked_accounts);?>
+                        <?php for($b=0;$b<count($lnk_array);$b++){
+                             if($lnk_array[$b]!=''){  ?>
+                        <h3 class="heading heading__4">Linked Account Holder: Name Name <?=getUserName($lnk_array[$b])?></h3>
+                        <div class="recess-box">
+                        <div class="account-table__head">
+                            <label>Client Code</label>
+                            <label>ISIN Code</label>
+                            <label>Designator</label>
+                            <label>Type</label>
+                            <label>Display Name</label>
+                        </div><!--head-->
+						<?php
 						  // Connect and create the PDO object
 						  $conn = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
 						  $conn->exec("SET CHARACTER SET $charset");      // Sets encoding UTF-8
@@ -182,104 +157,46 @@ require_once('page-sections/header-elements.php');
 
 						  // Parse returned data
 						  while($row = $result->fetch(PDO::FETCH_ASSOC)) { ?>
-							<tr>
-								<td><input type="text" id="fs_client_code" name="fs_client_code" style="width:90%" value="<?=$lnk_array[$b];?>" readonly></td>
-								<td><input type="text" id="fs_isin_code" name="fs_isin_code" style="width:90%" value="<?=$row['fs_isin_code'];?>" readonly></td>
-								<td><input type="text" id="fs_designation" name="fs_designation" style="width:90%" value="<?=$row['fs_designation'];?>" readonly>
-								<td><input type="text" id="fs_client_code" name="fs_client_code" style="width:90%" value="<?=$row['fs_product_type'];?>" readonly></td>
-								<td><input type="text" id="fs_client_code" name="fs_client_code" style="width:90%" value="<?=getUserName($lnk_array[$b]) . ' ' . $row['fs_product_type'];?>" readonly></td>
-							</tr>
+                            <!--For each row, fetch this-->
+                            <div class="account-table__body">
+    							<p><?=$lnk_array[$b];?></p>
+                                <p><?=$row['fs_isin_code'];?></p>
+                                <p><?=$row['fs_designation'];?></p>
+                                <p><?=$row['fs_product_type'];?></p>
+                                <p><?=getUserName($lnk_array[$b]) . ' ' . $row['fs_product_type'];?></p>
+                            </div><!--body-->
 						  <?php }
-
-						  $conn = null;        // Disconnect
-
+						  $conn = null; // Disconnect
 						}?>
-						</tbody>
-			  </table>
-                  <?php  }?>
+                    </div>
+                    <?php  }?>
+                    <?php }	?>
+                </div>
+            </div><!--content-->
+            <div class="control">
+                <h3 class="heading heading__2">Account Actions</h3>
+                <input type="submit" class="button button__raised" value="Save Changes">
+            </div>
+        </form>
 
-            <?php }	?>
+    </div>
+</div>
 
-		</div>
-
-        <div class="col-md-3" style="float:left;">
-            <h5>Client Actions</h5>
-            <input type="submit" class="btn btn-grey" value="Save Changes">
-        </div>
-
-</form>
-
-
-
-
-		<div id="assetdetails" class="col-md-12 mt-5"></div>
-
-        </main>
       </div>
     </div>
 
 
 
-<!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="index.php">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-<!-- Delete Modal-->
-  <div class="modal deletefund" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="deleteModal">Delete this Asset?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Continue" below if you are ready to<br>delete this Asset.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-danger btn-ok">Delete</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/@popperjs/core@2"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
-
-     <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <!-- Date Picker -->
-	<link rel="stylesheet" href="css/bootstrap-datepicker3.css">
-	<script src="js/bootstrap-datepicker.min.js"></script>
+    <?php
+    require_once('page-sections/footer-elements.php');
+    require_once('modals/delete.php');
+    require_once('modals/logout.php');
+    require_once('modals/delete-cat.php');
+    require_once(__ROOT__.'/global-scripts.php');?>
 
     <script>
       feather.replace()
     </script>
-
-
 
     <script>
 
