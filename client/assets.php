0659aece4026b4e8e3719b8089c4f978d167a2a5
Ash - SIlverless <ash@silverless.co.uk>
2020-05-06 14:24:15 +0100

    DEV - adjust client assets

--- client/assets.php
+++ client/assets.php
@@ -1,45 +1,45 @@
 <?php
 include 'inc/db.php';     # $host  -  $user  -  $pass  -  $db
-​
+
 /*
 ini_set ("display_errors", "1");
 error_reporting(E_ALL);
     */
-​
+
 $user_id = $_SESSION['fs_client_featherstone_uid'];
 $client_code = $_SESSION['fs_client_featherstone_cc'];
 $last_date = getLastDate('tbl_fs_transactions','fs_transaction_date','fs_transaction_date','fs_client_code = "'.$client_code.'"');
 $confirmed_date = $row['confirmed_date']= date('d M Y');
-​
+
 $lastlogin = date('g:ia \o\n D jS M y',strtotime(getLastDate('tbl_fsusers','last_logged_in','last_logged_in','id = "'.$_SESSION['fs_client_user_id'].'"')));
-​
+
 $strategy = 'fs_growth_'.strtolower(getField('tbl_fsusers','strategy','id',$_SESSION['fs_client_user_id']));
-​
-​
+
+
 try {
   // Connect and create the PDO object
   $conn = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
   $conn->exec("SET CHARACTER SET $charset");      // Sets encoding UTF-8
-​
+
     $query = "SELECT *  FROM `tbl_fs_assets` where $strategy > 0 AND bl_live = 1;";
-​
+
     $result = $conn->prepare($query);
     $result->execute();
-​
+
           // Parse returned data
           while($row = $result->fetch(PDO::FETCH_ASSOC)) {
 			 $assetData[] =  $row;
-​
+
         }
-​
+
   $conn = null;        // Disconnect
-​
+
 }
-​
+
 catch(PDOException $e) {
   echo $e->getMessage();
 }
-​
+
 ?>
 <?php
 define('__ROOT__', dirname(dirname(__FILE__)));
@@ -47,26 +47,26 @@ require_once(__ROOT__.'/header.php');
 require_once(__ROOT__.'/page-sections/header-elements.php');
 require_once(__ROOT__.'/page-sections/sidebar-elements.php');
 ?>
-​
+
 		    <div class="col-md-9">
-​
+
                 <div class="border-box main-content">
-​
+
                     <div class="main-content__head">
                         <h1 class="heading heading__1">Holdings & Asset Allocation</h1>
                         <p class="mb3">Data accurate as at <?= $confirmed_date;?></p>
                     </div>
-​
+
 <div class="asset-wrapper">
     <div class="asset-wrapper__chart">
-​
+
         <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut" aria-labelledby="" role="img" style="transform:rotate(-90deg);">
             <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#484848" role="presentation"></circle>
             <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#414141" stroke-width="10" role="presentation"></circle>
             <!--For each holding, create a segment like this
             Params =
             Stroke-dasharray: two figures.  The first is the value of the holding (ie, 30%); the second is the first value minus 100 (ie 30 - 100) therefore 70.
-​
+
             Stroke-dashoffset: This is the running sum of the value of the holding, expressed as a negative value to enable positioning.
             -->
             <?php foreach($assetData as $asset) {
@@ -77,7 +77,7 @@ require_once(__ROOT__.'/page-sections/sidebar-elements.php');
               $thisAsset = $asset[$strategy];
               $assetBalance = 100 - $thisAsset;
             ?>
-​
+
                <circle id="asset<?=$asset['id'];?>" class="donut-segment <?=$asset['id'];?> <?=$asset['fs_asset_name'];?> asset<?=$asset['id'];?>" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="<?= $asset_color;?>" stroke-width="10" stroke-dasharray="<?=$thisAsset;?> <?=$assetBalance;?>" stroke-dashoffset="-<?=$assetTotal;?>"></circle>
                <text x="22" y="22" text-anchor="middle" alignment-baseline="middle" class="asset<?=$asset['id'];?>"><?=$thisAsset;?>%</text>
                <?php $assetTotal = $thisAsset += $assetTotal;?>
@@ -125,8 +125,8 @@ require_once(__ROOT__.'/page-sections/sidebar-elements.php');
 </div>
     </div>
 </div>
-​
-​
+
+
 <!-- Logout Modal-->
   <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -145,7 +145,7 @@ require_once(__ROOT__.'/page-sections/sidebar-elements.php');
       </div>
     </div>
   </div>
-​
+
 <!--    Logged Out  -->
     <div class="modal fade" id="loggedout" tabindex="-1" role="dialog" aria-labelledby="LoggedOut" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -165,6 +165,6 @@ require_once(__ROOT__.'/page-sections/sidebar-elements.php');
 <?php define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/global-scripts.php');
 require_once('../page-sections/footer-elements.php');?>
-​
+
   </body>
 </html>
